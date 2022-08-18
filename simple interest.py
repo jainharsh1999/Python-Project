42c714676ef2010965fc39923c9636ccfe2385c7
jainharsh1999 <103169009+jainharsh1999@users.noreply.github.com>
2022-08-18 07:55:16 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,9 @@
+given_amount = 18816
+given_time = 3
+
+given_rate = 8.43
+
+simpleInt = (given_amount*given_time*given_rate)/100
+
+print("The simple interest for the amount", given_amount, "in",
+      given_time, "at the rate", given_rate, "% =", simpleInt)
